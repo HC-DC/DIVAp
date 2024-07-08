8a1cd540c94604f0f8d084e2a34394afcdf0c3d5
herve <herve.lecoroller@lam.fr>
2024-07-08 11:05:42 +0200

    Update soft to open and save hcifits files

--- open_diva_hcifits.py
+++ open_diva_hcifits.py
@@ -1,76 +1,158 @@
-import os
+from hcifits import HCIFits
+from astropy.table import Table
 from astropy.io import fits
 import numpy as np
 import pandas as pd
 
-def save_fits_image(hdu, path, name):
-    image_path = os.path.join(path, f"{name}.fits")
-    hdu.writeto(image_path, overwrite=True)
-    print(f"Saved image: {image_path}")
+def save_hcifits_files(path, name):
 
-def save_fits_table(hdu, path, name):
-    table_path = os.path.join(path, f"{name}.csv")
-    data = hdu.data
-    columns = hdu.columns.names
+    # read and save hcifits file
+    h = HCIFits.read(path + f"/{name}")
 
-    # Dictionary to store data columns
-    df_dict = {}
+    # read SNR MAP
+    snr_map = h.snr_map
+    # Check if h.snr_map is an ImageHDU object and save it to a FITS file
+    if isinstance(snr_map, fits.ImageHDU):
+        snr_map.writeto(path +'/SNR_MAP.fits', overwrite=True)
+    else:
+        print("h.snr_map is not an ImageHDU object")
 
-    for col in columns:
-        col_data = data[col]
-        if isinstance(col_data[0], np.ndarray):
-            # Merge multidimensional data into unique columns
-            for idx in range(col_data.shape[1]):
-                col_name = f"{col}_{idx}"
-                if col in df_dict:
-                    df_dict[col].extend([row[idx] for row in col_data])
-                else:
-                    df_dict[col_name] = [row[idx] for row in col_data]
-        else:
-            df_dict[col] = list(col_data)
+    # Read and save SENSITIVITY MAP
+    sensitivity_map = h.sensitivity_map
+    # Check if h.sensitivity_map is an ImageHDU object and save it to a FITS file
+    if isinstance(sensitivity_map, fits.ImageHDU):
+        sensitivity_map.writeto(path +'/SENSITIVITY_MAP.fits', overwrite=True)
+    else:
+        print("h.sensitivity_map is not an ImageHDU object")
 
-    # Find the maximum length of all columns
-    max_length = max(len(v) for v in df_dict.values())
+    # Read and save REDUCED image
+    reduced_data = h.reduced_data
+    # Check if h.reduced_data is an ImageHDU object and save it to a FITS file
+    if isinstance(reduced_data, fits.ImageHDU):
+        reduced_data.writeto(path + '/REDUCED_DATA.fits', overwrite=True)
+    else:
+        print("h.reduced_data is not an ImageHDU object")
 
-    # Ensure all columns have the same length
-    for col, values in df_dict.items():
-        if len(values) < max_length:
-            df_dict[col].extend([None] * (max_length - len(values)))
+    # Read and Save data_information table
+    data_info_table = Table.read(h.data_information)
+    # Extract Wavelength column from data_info_table
+    wavelengths = data_info_table['Wavelength']
+    nbr_wavelength = np.size(data_info_table['Wavelength'])
+    data_info_table.write(path + '/DATA_INFORMATION.csv', format='csv', overwrite=True)
 
-    # Create the DataFrame from the dictionary
-    df_final = pd.DataFrame(df_dict)
-    df_final.to_csv(table_path, index=False)
-    print(f"Saved table: {table_path}")
+    # Read and transform source_detection table
+    source_detection_table = Table.read(h.source_detection)
 
-def inspect_and_extract_fits_file(file_path, save_path):
-    # Open the FITS file
-    with fits.open(file_path) as hdul:
-        # Iterate through each HDU (Header/Data Unit) to process each type
-        for hdu in hdul:
-            hdu_type = type(hdu).__name__
-            hdu_name = hdu.name
+    # lists for the columns of the DataFrame
+    candidates = []
+    wavelength_list = []
+    snr_list = []
+    dra_list = []
+    err_dra_list = []
+    ddec_list = []
+    err_ddec_list = []
+    sep_list = []
+    err_sep_list = []
+    pa_list = []
+    err_pa_list = []
+    flux_cs_list = []
+    err_flux_cs_list = []
+    flux_mag_list = []
+    err_flux_mag_list = []
+    flux_jy_list = []
+    err_flux_jy_list = []
+    flux_erg_list = []
+    err_flux_erg_list = []
+    contrast_list = []
+    err_contrast_list = []
+    wp3_id_list = []
+    wp3_status_list = []
 
-            try:
-                if hdu_type == 'ImageHDU':
-                    save_fits_image(hdu, save_path, hdu_name)
-                elif hdu_type == 'BinTableHDU':
-                    save_fits_table(hdu, save_path, hdu_name)
-                elif hdu_type == 'PrimaryHDU':
-                    continue  # Skip PRIMARY HDU
-                else:
-                    print(f"Other HDU found: Name = {hdu_name}, Type = {hdu_type}")
-            except Exception as e:
-                print(f"WARNING: {hdu_name} extraction failed with error: {e}")
+    # Transform multidimensional to unidimensional columns
+    for row in source_detection_table:
+        for i in range(nbr_wavelength):
+            candidates.append(row['Candidate'])
+            wavelength_list.append(wavelengths[i])
+            snr_list.append(row['SNR'][i])
+            dra_list.append(row['dRA'][i])
+            err_dra_list.append(row['err_dRA'][i])
+            ddec_list.append(row['dDEC'][i])
+            err_ddec_list.append(row['err_dDEC'][i])
+            sep_list.append(row['Sep'][i])
+            err_sep_list.append(row['err_Sep'][i])
+            pa_list.append(row['PA'][i])
+            err_pa_list.append(row['err_PA'][i])
+            flux_cs_list.append(row['Flux_cs'][i])
+            err_flux_cs_list.append(row['err_Flux_cs'][i])
+            flux_mag_list.append(row['Flux_mag'][i])
+            err_flux_mag_list.append(row['err_Flux_mag'][i])
+            flux_jy_list.append(row['Flux_Jy'][i])
+            err_flux_jy_list.append(row['err_Flux_Jy'][i])
+            flux_erg_list.append(row['Flux_erg'][i])
+            err_flux_erg_list.append(row['err_Flux_erg'][i])
+            contrast_list.append(row['Contrast'][i])
+            err_contrast_list.append(row['err_Contrast'][i])
+            wp3_id_list.append(row['WP3_ID'])
+            wp3_status_list.append(row['WP3_Status'])
 
-        # Check for expected elements and print a warning if any are missing
-        expected_elements = ['DATA_INFORMATION', 'REDUCED_DATA', 'SNR_MAP', 'SENSITIVITY_MAP', 'DETECTION_LIMIT', 'SOURCE_DETECTION']
-        for element in expected_elements:
-            if element not in [hdu.name for hdu in hdul]:
-                print(f"WARNING: {element} is missing")
+    # Creat a pandas DataFrame
+    df = pd.DataFrame({
+        'Candidate': candidates,
+        'Wavelength': wavelength_list,
+        'SNR': snr_list,
+        'dRA': dra_list,
+        'err_dRA': err_dra_list,
+        'dDEC': ddec_list,
+        'err_dDEC': err_ddec_list,
+        'Sep': sep_list,
+        'err_Sep': err_sep_list,
+        'PA': pa_list,
+        'err_PA': err_pa_list,
+        'Flux_cs': flux_cs_list,
+        'err_Flux_cs': err_flux_cs_list,
+        'Flux_mag': flux_mag_list,
+        'err_Flux_mag': err_flux_mag_list,
+        'Flux_Jy': flux_jy_list,
+        'err_Flux_Jy': err_flux_jy_list,
+        'Flux_erg': flux_erg_list,
+        'err_Flux_erg': err_flux_erg_list,
+        'Contrast': contrast_list,
+        'err_Contrast': err_contrast_list,
+        'WP3_ID': wp3_id_list,
+        'WP3_Status': wp3_status_list
+    })
+    # Save the source_detection table
+    df.to_csv(path + '/SOURCE_DETECTION.csv', index=False)
 
+    # Read DETECTION_LIMIT table
+    detection_limit_table = Table.read(h.detection_limit)
+    nbr_radius = len(detection_limit_table['Radius'][0])
 
-# path to your FITS file and the save directory
-PATH = '/Users/herve/Documents/recherche/tmp/'
-file_path = PATH + 'HIP_114189_DC_id_151321.fits'
-save_path = PATH
-inspect_and_extract_fits_file(file_path, save_path)
+    # lists for the columns of the DataFrame
+    radius = []
+    wavelength_list = []
+    detection_Limit = []
+
+    # Transform multidimensional to unidimensional columns
+    for row in detection_limit_table:
+        for i in range(nbr_wavelength):
+            for j in range(nbr_radius):
+                radius.append(row['Radius'][j])
+                wavelength_list.append(wavelengths[i])
+                detection_Limit.append(row['Detection_Limit'][j])
+
+    # Creat a pandas DataFrame
+    df = pd.DataFrame({
+        'Radius': radius,
+        'Wavelength': wavelength_list,
+        'Detection_Limit': detection_Limit,
+    })
+    # Save the detection limit table
+    df.to_csv(path + '/DETECTION_LIMIT.csv', index=False)
+
+def main():
+    path = '/Users/herve/Documents/recherche/tmp/'
+    hcifits_name = 'HIP11360_DC_id_567465.fits'
+    save_hcifits_files(path, hcifits_name)
+if __name__ == "__main__":
+    main()
